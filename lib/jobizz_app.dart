1463b4d33475dcd105bba66849cf5ede474536cd
abdulrahman <abdulrahmanramadan22@gmail.com>
2025-05-07 03:32:27 +0300

    structure project

---
+++ lib/jobizz_app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class JobizzApp extends StatelessWidget {
+  const JobizzApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+         title: 'Jobizz App',
+        theme: ThemeData(
+          // fontFamily: 'Poppins',
+        
+        ),
+    );
+  }
+}
