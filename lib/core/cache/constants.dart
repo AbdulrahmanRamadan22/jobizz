b9b66609779b377d98c0ec5793112a04329259b5
abdulrahman <abdulrahmanramadan22@gmail.com>
2025-05-08 18:51:33 +0300

    -added SharedPrefHelper

---
+++ lib/core/cache/constants.dart
@@ -0,0 +1,22 @@
+class SharedPrefKeys {
+  static String token = "token";
+  static String fullName = "fullName";
+  static String email = "email";
+  static String phone = "phone";
+  static String status = "status";
+  static String isLoggedInUser = "isLoggedInUser";
+  static String tokenResetPassword = "token";
+  static String emailResetPassword = "emailResetPassword";
+}
+
+class SharedPrefValues {
+  static String? token;
+  static String username = "";
+  static String fullName = "";
+  static String email = "";
+  static String phone = "";
+  static String status = "";
+  static bool? isLoggedInUser = false;
+  static String? tokenResetPassword = "";
+  static String? emailResetPassword = "";
+}
