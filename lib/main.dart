8aae8d04cf4e61063742b7f2e53f5d65c618ddb3
abdulrahman <abdulrahmanramadan22@gmail.com>
2025-05-08 19:08:18 +0300

    added setupGetIt -added MyBlocObserver -added MyBlocObserver -added ApiErrorHandler -added ApiErrorModel -added DioFactory -added ApiResult -added ApisServices

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,30 @@
+import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:jobizz/core/helper/bloc_observer.dart';
 import 'package:jobizz/core/routing/app_router.dart';
 
+import 'core/cache/shared_pref.dart';
+import 'core/di/dependancy_ingection.dart';
 import 'jobizz_app.dart';
 
-void main() {
+void main() async{
+
+
+ WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize shared preferences
+  await  SharedPrefHelper.init();
+  // Initialize dependency injection
+  setupGetIt();
+
+  Bloc.observer = MyBlocObserver();
+  // Check login status
+  // final bool isLoggedIn = SharedPrefHelper.isLoggedIn();
+  // // final String initialRoute =
+  //     isLoggedIn ? Routes.homeScreen : Routes.loginScreen;
+
+
+
   runApp(JobizzApp(
     appRouter: AppRouter(),
   ));
