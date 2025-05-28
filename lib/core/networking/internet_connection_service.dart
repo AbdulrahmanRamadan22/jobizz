b485a07ede005b59809c0b5499aa7b8c4b18d6b1
abdulrahman <abdulrahmanramadan22@gmail.com>
2025-05-28 07:38:54 +0300

    update

--- lib/core/networking/internet_connection_service.dart
+++ lib/core/networking/internet_connection_service.dart
@@ -1,58 +1,7 @@
-// import 'dart:async';
-// import 'package:connectivity_plus/connectivity_plus.dart';
-// import 'package:internet_connection_checker/internet_connection_checker.dart';
-
-// class InternetConnectionService {
-//   static final InternetConnectionService _instance =
-//       InternetConnectionService._internal();
-//   factory InternetConnectionService() => _instance;
-
-//   final Connectivity _connectivity = Connectivity();
-//   final InternetConnectionChecker _internetChecker =
-//       InternetConnectionChecker.instance;
-
-//   final StreamController<bool> _connectionChangeController =
-//       StreamController<bool>.broadcast();
-
-//   bool _hasConnection = false;
-
-//   InternetConnectionService._internal() {
-//     _initialize();
-//   }
-
-//   void _initialize() async {
-//     _hasConnection = await _checkInternet();
-
-//     _connectivity.onConnectivityChanged.listen((List<ConnectivityResult> results) {
-//       for (var result in results) {
-//         _updateConnectionStatus(result);
-//       }
-//     });
-//   }
-
-//   Stream<bool> get connectionChange => _connectionChangeController.stream;
-//   bool get hasConnection => _hasConnection;
-
-//   Future<void> _updateConnectionStatus(ConnectivityResult result) async {
-//     bool previousConnection = _hasConnection;
-//     bool currentConnection = await _checkInternet();
-//     if (currentConnection != previousConnection) {
-//       _hasConnection = currentConnection;
-//       _connectionChangeController.add(_hasConnection);
-//     }
-//   }
-
-//   Future<bool> _checkInternet() async {
-//     return await _internetChecker.hasConnection;
-//   }
-
-//   void dispose() {
-//     _connectionChangeController.close();
-//   }
-// }
 
 import 'dart:async';
 import 'dart:io';
+
 import 'package:connectivity_plus/connectivity_plus.dart';
 
 class InternetConnectionService {
